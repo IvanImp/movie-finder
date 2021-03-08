e07619573e2facf0f4e43cb8ca80c1adee389087
Unknown <ivan_leong_97@hotmail.com>
2021-03-08 12:52:37 +0800

    Added basic search bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="search-bar">
+          <form>
+            <label>
+              <input className="search-input" placeholder="Search Movie..." type="text" name="movie" />
+            </label>
+          </form>
+
+          <button className="search-button">
+            Search
+          </button>
+        </div>
       </header>
     </div>
   );
